41787ec236015b0323ebc566706c0d862dcb99a7
Man Sarsale <johnemmansarsale@gmail.com>
2025-12-17 15:21:02 +0800

    Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Main Page</title>
-  <link rel="stylesheet" href="course-2.css">
+  <link rel="stylesheet" href="css/vbhc_101/course-2.css">
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
     rel="stylesheet" />
